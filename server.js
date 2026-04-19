199bb4860525d4ab0a1f6d2821df1ab54b162d3d
Kurt DEV <kurt.vonschaeffer@mymint.co.za>
2026-04-19 22:49:16 +0200

    Fix vercel build by removing static path dependency for dotenv

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+require('dotenv').config();
 const http = require('http');
 const https = require('https');
 const crypto = require('crypto');
